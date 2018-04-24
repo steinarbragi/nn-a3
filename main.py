b3df3ed37a9e903ccb92a28e43bf66afe538226e
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2018-04-24 11:00:54 +0200

    just following that blogpost to get started with real time object recognition

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# dataset: http://www.vision.caltech.edu/Image_Datasets/Caltech101/
+
+# blogpost: https://chunml.github.io/ChunML.github.io/project/Real-Time-Object-Recognition-part-one/
+
+
+import argparse
+import os
+import random
+import numpy as np
+import cv2
+from keras.models import Model
+from keras.applications import applications
+from keras.preprocessing import image as image_utils
+from keras.applications.vgg16 import preprocess_input
+
+
+model = applications.vgg16.VGG16(weights="imagenet")
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--folder", required=True)
+    args = vars(ap.parse_args())
+    files = [os.path.join(args["folder"], f) for f in os.listdir(args["folder"])]
+    random.shuffle(files)
+
+    for file in files:
+        #image = image_utils.load_img(file, target_size=(224, 224))
+        #image = image_utils.img_to_array(image)
+        image = cv2.imread(file)
+        image = image.transpose((2, 0, 1))
+        image = preprocess_input(image)
+
+
+if __name__ == '__main__':
+    main()
